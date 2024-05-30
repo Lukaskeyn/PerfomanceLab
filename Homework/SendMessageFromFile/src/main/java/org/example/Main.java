e94bd2cfc1eb734acfec4cd717e431bee6408ba4
lukke <lukkeyn33@gmail.com>
2024-05-30 15:32:56 +0300

    add dependency

--- Homework/SendMessageFromFile/src/main/java/org/example/Main.java
+++ Homework/SendMessageFromFile/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import ru.pflb.mq.dummy.implementation.DestinationImpl;
 import ru.pflb.mq.dummy.implementation.ProducerImpl;
 import ru.pflb.mq.dummy.interfaces.Connection;
 import ru.pflb.mq.dummy.interfaces.Session;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.concurrent.TimeUnit;
